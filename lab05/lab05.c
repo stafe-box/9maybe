38b26112f33085ff14383e70efa08a9d393b7e16
Eduard <edik.yunusov2016@mail.ru>
2021-05-31 01:53:51 +0500

    5 лаба готова

--- lab05/lab05.c
+++ lab05/lab05.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-double res1(unsigned int x)
+unsigned int res1(unsigned int x)
 {
-    int l = 0;
-    double h = 0;
-    while (x)
+    unsigned k = 0;
+    while(x)
     {
-        l++;
-        h += x % 10;
+        k+= x % 10;
         x /= 10;
     }
-    return h / (l * 1.0f);
+    return k;
 }
 
-double res2(unsigned int x, int *p)
+unsigned int res2(unsigned int x)
 {
-    
+    if (x)
+        return x % 10 + res2(x / 10);
+    else
+        return 0;
 }
 
 int main()
 {
-    unsigned int x = 0;
-    int k = 0;
+    unsigned int x = 0, k = 0, l = 0;
     printf("Введите число:");
     scanf("%u", &x);
-    printf("Средне арифметическое суммы цифр числа x: %le \n", res1(x));
-    printf("Средне арифметическое суммы цифр числа x: %le \n", res2(x, &k) / (1.0f * k));
+    l = x;
+    while(l)
+    {
+      k++;
+      l /= 10;
+    }
+    printf("Среднее арифметическое цифр числа через цикл: %le\n", (res1(x) * 1.0f) / (k * 1.0f));
+    printf("Среднее арифметическое цифр числа через рекурсию: %le\n", (res2(x) * 1.0f ) / (k * 1.0f));
+    printf("\n");
     return 0;
 }
